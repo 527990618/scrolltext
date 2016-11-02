c97f83406abaa0bcaa7b53f0e9df7345e0571249
Kevin Wang <KevinWang@mintcode.com>
2016-11-02 18:14:12 +0800

    'update'

--- js/scroll.js
+++ js/scroll.js
@@ -7,14 +7,33 @@
             this.element = elm;
             this.parentNode = elm.parent();
             this.direction = 'up' ? true : false;
+            this.items = this.element.children();
+            this.init();
         }
         //此处改为jquery的extend方法
         ScrollText.prototype = {
             init: function(){
-
+                this._scroll();
             },
             changeDirection: function () {
                 return this.direction = !this.direction;
+            },
+            _scroll: function () {
+                setInterval(this._scrollAnimate.bind(this), 5000);
+            },
+            _scrollAnimate: function(){
+                var me = this,
+                    firstItem = me.element.children().eq(0),
+                    scrollHeight = firstItem.outerHeight();
+                me.element[0].style.transform = 'translateY(' + (-scrollHeight) + 'px) translateZ(0)';
+                me.element.on('transitionend', function () {
+                    this.style.transitionDuration = '0s';
+                    me.element.children()[0].remove();
+                    me.element[0].style.transform = 'translateY(' + 0 + 'px) translateZ(0)';
+                    me.element.append(firstItem);
+                    // this.style.transitionDuration = '2s';
+                    me.element.off('transitionend');
+                })
             }
         };
         return ScrollText;
@@ -31,7 +50,7 @@
         })
     }
     $.fn.ScrollText.defaults = {
-        direction: up, //默认向上滚动
+        direction: 'up', //默认向上滚动
         stopEvent: false
     }
-})();
+})(jQuery);
